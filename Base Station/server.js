621ccf85c254a92628a74dd18cd74da48355269b
Joshua Nafziger <joshua9three@gmail.com>
2024-08-31 22:02:32 -0400

    Cleaned Server

--- Base Station/server.js
+++ Base Station/server.js
@@ -2,5 +2,9 @@ const express = require('express');
 const app = express();
 const port = 80;
 
+// Serve static files from the "public" directory
 app.use(express.static('front_end'));
-app.listen(port, () => {});
+
+app.listen(port, () => {
+  console.log(`Server Started on port ${port}`);
+});
