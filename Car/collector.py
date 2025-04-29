9f2d387f64c0af4cb977848fba1322de7716075d
Jared Mayfield <jared@csmayfield.org>
2025-04-29 19:45:27 -0400

    Reviewed and uploaded Josh's code for the car.

--- Car/collector.py
+++ Car/collector.py
@@ -4,19 +4,27 @@ import RPi.GPIO as GPIO  # type: ignore
 from adafruit_ads1x15.analog_in import AnalogIn # type: ignore
 import board # type: ignore
 from adafruit_lsm6ds.lsm6dsox import LSM6DSOX # type: ignore
-from adafruit_ads1x15.ads1115 import ADS # type: ignore
+import adafruit_ads1x15.ads1115 as ADS # type: ignore
+import adafruit_rfm9x # type: ignore
 
 from cc1101 import CC1101 # type: ignore
 from cc1101.config import TXConfig, Modulation # type: ignore
+from digitalio import DigitalInOut, Direction, Pull # type: ignore
 
 import math
 import subprocess
 import time
+from datetime import datetime, timedelta
 import logging
 import struct
 import sqlite3
 
-print("I guess all of the packages loaded! (:")
+import smbus # type: ignore
+
+rfm9x = adafruit_rfm9x.RFM9x(busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO), DigitalInOut(board.CE1), DigitalInOut(board.D25), 433.0, high_power = True)
+rfm9x.tx_power = 23
+
+bus = smbus.SMBus(1)
 
 On_GPS_time = False
 send_cooldown = 2 # With data pulling 4x a second, sending rf 2x a second
@@ -28,7 +36,7 @@ cur = con.cursor()
 # Create the initial table (Named main)
 cur.execute('''
 CREATE TABLE IF NOT EXISTS main (
-    time REAL UNIQUE PRIMARY KEY,,
+    time REAL UNIQUE PRIMARY KEY,
     amp_hours REAL,
     voltage REAL,
     current REAL,
@@ -47,17 +55,17 @@ CREATE TABLE IF NOT EXISTS main (
 ''')
 con.commit()
 
-tx_config = TXConfig.new(
-    frequency=915,
-    modulation=Modulation.MSK, # Read up: https://en.wikipedia.org/wiki/Minimum-shift_keying
-    baud_rate=12, # Baud rate in kbps (Currently 3kb for each quarter second packet)
-    sync_word=0xD391, # Unique 16-bit sync word (Happens to be unicode for 펑 :) )
-    preamble_length=4, # Recommended: https://e2e.ti.com/support/wireless-connectivity/sub-1-ghz-group/sub-1-ghz/f/sub-1-ghz-forum/1027627/cc1101-preamble-sync-word-quality
-    packet_length=120, # In Bytes (Number of columns * 8)
-    tx_power=12, # dBm (Currently the max, expected to use 34.2 mA)
-    crc = True, # Enable a checksum
-)
-radio = CC1101("/dev/cc1101.0.0") # The default device path
+# tx_config = TXConfig.new(
+#     frequency=915,
+#     modulation=Modulation.MSK, # Read up: https://en.wikipedia.org/wiki/Minimum-shift_keying
+#     baud_rate=12, # Baud rate in kbps (Currently 3kb for each quarter second packet)
+#     sync_word=0xD391, # Unique 16-bit sync word (Happens to be unicode for 펑 :) )
+#     preamble_length=4, # Recommended: https://e2e.ti.com/support/wireless-connectivity/sub-1-ghz-group/sub-1-ghz/f/sub-1-ghz-forum/1027627/cc1101-preamble-sync-word-quality
+#     packet_length=120, # In Bytes (Number of columns * 8)
+#     tx_power=0.1, # dBm (Currently the max, expected to use 34.2 mA)
+#     crc = True, # Enable a checksum
+# )
+# radio = CC1101("/dev/cc1101.0.0") # The default device path
 
 # Setup Thermistor Values
 R1 = 10000.0
@@ -67,15 +75,15 @@ c2 = 2.378405444e-04
 c3 = 2.019202697e-07
 
 # Setup Send LED
-sendLED = 24
+sendLED = 27
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(sendLED, GPIO.OUT)
 GPIO.output(sendLED, 0)
 
 # Setup i2C & Devices
 i2c = busio.I2C(board.SCL, board.SDA)
-analogA = ADS.ADS1115(i2c, address = 0x4A)
-analogB = ADS.ADS1115(i2c, address = 0x4B)
+analogA = ADS.ADS1115(i2c, address = 0x4B)
+analogB = ADS.ADS1115(i2c, address = 0x4A)
 CA704_ADDR = 0x28  # Replace with the actual I2C address -- Cycle Analyst
 GPS704_ADDR = 0x29  # Replace with the actual I2C address -- GPS
 
@@ -83,17 +91,17 @@ GPS704_ADDR = 0x29  # Replace with the actual I2C address -- GPS
 # https://chatgpt.com/share/673cbcb7-9a74-8010-9a24-c0a5603eb385
 
 # Setup Analog In Ports
-A0 = AnalogIn(analogA, ADS.P0) # throttle
-A1 = AnalogIn(analogA, ADS.P1) # motorTemp
-A2 = AnalogIn(analogA, ADS.P2) # battTemp1
-A3 = AnalogIn(analogA, ADS.P3) # battTemp2
-B0 = AnalogIn(analogB, ADS.P0) # battTemp3
-B1 = AnalogIn(analogB, ADS.P1) # battTemp4
+A0 = AnalogIn(analogA, ADS.P0) # battTemp1 
+A1 = AnalogIn(analogA, ADS.P1) # battTemp2 
+A2 = AnalogIn(analogA, ADS.P2) # battTemp3
+A3 = AnalogIn(analogA, ADS.P3) # battTemp4
+B0 = AnalogIn(analogB, ADS.P0) # motorTemp
+B1 = AnalogIn(analogB, ADS.P1) # throttle
 #B2 = AnalogIn(analogB, ADS.P2) # Port not used
 B3 = AnalogIn(analogB, ADS.P3) # brake
 
 # Setup UART for Cycle Anyalist
-cycleAnalyst = serial.Serial('/dev/serial0',baudrate=9600)
+cycleAnalyst = serial.Serial('/dev/serial0',baudrate=9600,timeout=5)
 
 
 # Function to write to a specific SC18IM704 UART (Used only if needed by user)
@@ -126,17 +134,25 @@ def read_from_uart(device_addr, length=10):
 def UART_CA():
     # Input from CA is: amp_hours|voltage|current|speed|miles
     try:
-        data = read_from_uart(CA704_ADDR, 10)  # Adjust length for Cycle Analyst
-        if data:
-            data = data.strip()
-            amp_hours, voltage, current, speed, miles = data.split('|')
-            return amp_hours, voltage, current, speed, miles
+        # data = read_from_uart(CA704_ADDR, 10)  # Adjust length for Cycle Analyst
+
+        if cycleAnalyst.in_waiting > 0:
+            line = cycleAnalyst.readline().decode('utf-8').strip()
+            if line:
+                amp_hours, voltage, current, speed, miles = line.split('\t')
+                amp_hours = float(amp_hours)
+                voltage = float(voltage)
+                current = float(current)
+                speed = float(speed)
+                miles = float(miles)
+                
+                return amp_hours, voltage, current, speed, miles
         else:
-            amp_hours = voltage = current = speed = miles = float('nan')
-            return  amp_hours, voltage, current, speed, miles
+            
+            return float('nan'), float('nan'), float('nan'), float('nan'), float('nan')
     except Exception as e:
         print(f"Error in UART_CA function: {e}")
-        return float('nan')
+        return float('nan'), float('nan'), float('nan'), float('nan'), float('nan')
 
 
 def set_system_time(timestamp, date):
@@ -147,6 +163,9 @@ def set_system_time(timestamp, date):
     minutes = timestamp[2:4]
     seconds = timestamp[4:]
 
+    # Adjust for our timezone
+    hours = int(hours)  - 4
+
     # Extract day, month, and year from date
     day = date[:2]
     month = date[2:4]
@@ -156,12 +175,29 @@ def set_system_time(timestamp, date):
     formatted_time = f"{year}-{month}-{day} {hours}:{minutes}:{seconds}"
 
     # Set the system time using the `date` command
-    subprocess.run(["sudo date -s", formatted_time], check=True)
-    print("System time sucessfully set to GPS time:", formatted_time)
+    subprocess.run(["sudo", "date", "-s", formatted_time], check=True)
+    print("System time sucessfully set to GPS time:", datetime.now())
 
     global On_GPS_time
     On_GPS_time = True
 
+def read_full_sentence():
+    sentence = ""
+    for _ in range(10):
+        try:
+            data = bus.read_i2c_block_data(0x42, 0, 32)
+            valid_data = ''.join([chr(i) for i in data if 32 <= i <= 126 or i in (10, 13)])
+            sentence += valid_data
+            if '\n' in valid_data:
+                break
+        except OSError as e:
+            if e.errno == 5:
+                print("ESP not online")
+            else:
+                print("Error:", e)
+            break
+        time.sleep(0.01)
+    return sentence
 
 # UART handler for GPS
 def UART_GPS():
@@ -169,64 +205,51 @@ def UART_GPS():
     # The $GPRMC scheme containes the most relevant data
     # $GPRMC documentation: https://docs.novatel.com/OEM7/Content/Logs/GPRMC.htm
     try:
-        data = read_from_uart(GPS704_ADDR, 128)  # GPS data length can be longer (up to 255 bytes)
-        if not data:
-            print("No data received from GPS")
-            GPS_x = GPS_y = float('nan')
+        data = read_full_sentence()
+
+        # data = read_from_uart(GPS704_ADDR, 128)  # GPS data length can be longer (up to 255 bytes)
+        if data:
+
+            killall, timestamp, pos_status, lat, lat_dir, lon, lon_dir, speed, track_true, date, \
+                mag_var, var_dir, checksum = data.split(',')
+
+            # If no GPS fix, return nan for all variables
+            if pos_status == 'V':
+                print("No GPS fix!!!")
+                return float('nan'), float('nan')
+
+            # Set System time to gps time if not done yet
+            if not On_GPS_time:
+                set_system_time(timestamp, date)
+
+            # Convert latitude and longitude to decimal degrees
+            lat = float(lat[:2]) + float(lat[2:]) / 60.0
+            if lat_dir == 'S':
+                lat = -lat
+
+            lon = float(lon[:3]) + float(lon[3:]) / 60.0
+            if lon_dir == 'W':
+                lon = -lon
+
+            # Convert decimal degrees to radians
+            lat = math.radians(lat)
+            lon = math.radians(lon)
+
+            # The IAU nominal "zero tide" equatorial radius of the Earth
+            R = 6378100
+
+            # Convert to Cartesian coordinates
+            GPS_x = R * math.cos(lat) * math.cos(lon)
+            GPS_y = R * math.cos(lat) * math.sin(lon)
+
+            # Success!
             return GPS_x, GPS_y
-
-        data = data.strip()
-        if "$GPRMC" not in data:
-            print("No $GPRMC found")
-            GPS_x = GPS_y = float('nan')
-            return GPS_x, GPS_y
-
-        data = data.split("$GPRMC")[1]
-        data = data.split("\r\n")[0]
-
-        timestamp, pos_status, lat, lat_dir, lon, lon_dir, speed, track_true, date, \
-            mag_var, var_dir, mod_ind, checksum = data.split(',')
-
-        # If no GPS fix, return nan for all variables
-        if pos_status == 'V':
-            print("No GPS fix!!!")
-            GPS_x = GPS_y = float('nan')
-            return GPS_x, GPS_y
-
-        # Set System time to gps time if not done yet
-        if not On_GPS_time:
-            set_system_time(timestamp, date)
-
-        # Convert latitude and longitude to decimal degrees
-        lat = float(lat[:2]) + float(lat[2:]) / 60.0
-        if lat_dir == 'S':
-            lat = -lat
-
-        lon = float(lon[:3]) + float(lon[3:]) / 60.0
-        if lon_dir == 'W':
-            lon = -lon
-
-        # Convert decimal degrees to radians
-        lat = math.radians(lat)
-        lon = math.radians(lon)
-
-        # The IAU nominal "zero tide" equatorial radius of the Earth
-        R = 6378100
-
-        # Convert to Cartesian coordinates
-        GPS_x = R * math.cos(lat) * math.cos(lon)
-        GPS_y = R * math.cos(lat) * math.sin(lon)
-
-        # Success!
-        return GPS_x, GPS_y
 
     except Exception as e:
         print(f"Error in UART_GPS function: {e}")
 
     # didn't work out
-    GPS_x = GPS_y = float('nan')
-    return GPS_x, GPS_y
-
+    return float('nan'), float('nan')
 
 def thermistor(idx):
     R2 = R1 * (1023.0 / float(idx) - 1.0)
@@ -275,7 +298,7 @@ def analogPull():
 
     # Battery 4 Temperature
     try:
-        batt_4 = thermistor(B1.value)
+        batt_4 = A3.value
     except:
         batt_4 = float('nan')
 
@@ -283,9 +306,9 @@ def analogPull():
 
 insert_data_sql = """
     INSERT INTO main (
-        time, Throttle, Brake_Pedal, Motor_temp, Battery_1, Battery_2, Battery_3, Battery_4,
-        ca_AmpHrs, ca_Voltage, ca_Current, ca_Speed, ca_Miles, GPS_X, GPS_Y
-    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        time, Throttle, Brake, Motor_temp, Batt_1, Batt_2, Batt_3, Batt_4,
+        amp_hours, Voltage, Current, Speed, Miles, GPS_X, GPS_Y
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     """
 
 def mainloop():
@@ -307,6 +330,7 @@ def mainloop():
          amp_hours, voltage, current, speed, miles, GPS_x, GPS_y
         ]
 
+        print(data)
         # Add data to the database:
         cur.execute(insert_data_sql, data)
         con.commit()
@@ -320,14 +344,15 @@ def mainloop():
 
                 # Send Data
                 GPIO.output(sendLED, 1)
-                radio.transmit(tx_config, data_2_send)
-                GPIO.output(sendLED, 0)
+
+                packed_data = struct.pack('<' + 'd'*len(data), *data)
+                rfm9x.send(packed_data)
                 print("Packet sent")
+                # radio.transmit(tx_config, data_2_send)
+                GPIO.output(sendLED, 0)
 
             except Exception as e:
                 print("Error sending data:", e)
-        
-            send_cooldown = 2
 
         time.sleep(0.25)
 
