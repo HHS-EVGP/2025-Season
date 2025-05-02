2d8b7b4fcecb7c027171b70ed843fa6f27b9ee50
Jared Mayfield <jared@csmayfield.org>
2025-05-01 23:59:14 -0400

    Database creation for socket dummy.

--- BaseStation/SupportingFiles/socket_dummy.py
+++ BaseStation/SupportingFiles/socket_dummy.py
@@ -3,6 +3,36 @@ import pickle
 import os
 import random
 import time
+import sqlite3
+
+# Link the database to the python cursor
+con = sqlite3.connect("../EVGPTelemetry.sqlite")
+cur = con.cursor()
+
+# If main table does not exist as a table, create it
+create_table_sql = """
+    CREATE TABLE IF NOT EXISTS main (
+    time REAL UNIQUE PRIMARY KEY,
+    amp_hours REAL,
+    voltage REAL,
+    current REAL,
+    speed REAL,
+    miles REAL,
+    throttle REAL,
+    brake REAL,
+    motor_temp REAL,
+    batt_1 REAL,
+    batt_2 REAL,
+    batt_3 REAL,
+    batt_4 REAL,
+    GPS_x REAL,
+    GPS_y REAL,
+    laps NUMERIC
+)
+"""
+cur.execute(create_table_sql)
+con.commit()
+con.close()
 
 SOCKETPATH = "/tmp/telemSocket"
 
